ec2c66da49d5a9f174fde65c543c131eb9c55d44
Erick Amorim <erickamorimcarv@gmail.com>
2022-06-04 22:40:00 -0300

    finalizado

---
+++ telas/cadastro.php
@@ -0,0 +1,11 @@
+<h1>Cadastro de Contatos</h1>
+<hr>
+<form action="" method="post">
+    <input name="nome" type="text" class="form-control mt-2" type="text" placeholder="Nome"/>
+
+    <input name="email" type="text" class="form-control mt-2" type="text" placeholder="Email"/>
+
+    <input name="telefone" type="text" class="form-control mt-2" type="text" placeholder="Telefone"/>
+    
+    <button class="btn btn-primary mt-3">Enviar</button>
+</form>
